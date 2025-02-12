06db49c594796c943b233d28e44bef045f05d9d3
AJ Meireles <alvaro.meireles@live.com>
2025-02-12 10:47:09 -0300

    introducing scroll-on-mobile to the tab component

--- src/resources/views/components/tab/tab.blade.php
+++ src/resources/views/components/tab/tab.blade.php
@@ -3,14 +3,16 @@
 @endphp
 
 <div x-data="{ selected: @if (!$selected) {!! TallStackUi::blade($attributes, $livewire)->entangle() !!} @else @js($selected) @endif, tabs: [] }" class="{{ $personalize['base.wrapper'] }}">
-    <div class="{{ $personalize['base.padding'] }}">
-        <select x-model="selected" class="{{ $personalize['base.select'] }}" aria-label="Select a tab" x-on:change="$refs.ul.dispatchEvent(new CustomEvent('navigate', {detail: {select: selected}}));">
-            <template x-for="item in tabs">
-                <option x-bind:value="item.tab" x-text="item.tab" x-bind:selected="item.tab === selected"></option>
-            </template>
-        </select>
-    </div>
-    <ul role="tablist" class="{{ $personalize['base.body'] }}" {{ $attributes->only('x-on:navigate') }} x-ref="ul">
+    @if (!$scrollOnMobile)
+        <div class="{{ $personalize['base.padding'] }}">
+            <select x-model="selected" class="{{ $personalize['base.select'] }}" aria-label="Select a tab" x-on:change="$refs.ul.dispatchEvent(new CustomEvent('navigate', {detail: {select: selected}}));">
+                <template x-for="item in tabs">
+                    <option x-bind:value="item.tab" x-text="item.tab" x-bind:selected="item.tab === selected"></option>
+                </template>
+            </select>
+        </div>
+    @endif
+    <ul role="tablist" @class([$personalize['base.body'], 'hidden sm:flex' => ! $scrollOnMobile]) {{ $attributes->only('x-on:navigate') }} x-ref="ul">
         <template x-for="item in tabs">
             <li role="tab"
                 tabindex="0"
@@ -19,7 +21,8 @@
                 x-bind:aria-selected="selected === item.tab ? 'true' : 'false'"
                 x-bind:class="{
                     '{{ $personalize['item.select'] }}' : selected === item.tab,
-                    '{{ $personalize['item.unselect'] }}' : selected !== item.tab
+                    '{{ $personalize['item.unselect'] }}' : selected !== item.tab,
+                    'hidden sm:flex': selected !== item.tab && ! @js($scrollOnMobile),
                 }">
                 <div class="{{ $personalize['item.wrapper'] }}">
                     <template x-if="item.left">
@@ -33,7 +36,7 @@
             </li>
         </template>
     </ul>
-    <hr class="{{ $personalize['base.divider'] }}">
+    <hr @class([$personalize['base.divider'], 'hidden sm:block' => ! $scrollOnMobile])>
     <div class="{{ $personalize['base.content'] }}">
         {{ $slot }}
     </div>
